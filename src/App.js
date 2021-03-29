eac7fc2caa67b099238c13a796f72961ba324547
Elin Amren <elin.amren@gmail.com>
2021-03-29 15:50:59 +0200

    Get react user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const users = [
+  {
+    name: "Holly",
+    image: "👩🏽‍🔬",
+    title: "Scientist",
+  },
+  {
+    name: "Janet",
+    image: "👩🏻‍🎤",
+    title: "Pilot",
+  },
+  {
+    name: "Henry",
+    image: "🦹🏻‍♂️",
+    title: "Hairdresser",
+  },
+  {
+    name: "Nick",
+    image: "🤖",
+    title: "Doctor",
+  },
+];
 
 function App() {
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function getUser() {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setUser(users[Math.floor(Math.random() * users.length)]);
+    }, 3000);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="user">
+        {loading ? (
+          <p>Looking for the best user for you.. just a second!</p>
+        ) : (
+          <>
+            <h1>{user.name}</h1>
+            <h2>{user.image}</h2>
+            <p>{user.title}</p>
+          </>
+        )}
+      </div>
+      <button onClick={getUser}>Get new user</button>
     </div>
   );
 }
